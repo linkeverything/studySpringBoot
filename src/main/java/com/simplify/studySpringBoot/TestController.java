fcf4fa372a0b37beafc12f38e0797b8b20bd7893
Jonghun Park <jonghun.criss.android@gmail.com>
2020-12-23 15:16:43 +0900

    0010-create-api
    Signed-off-by: Jonghun Park <jonghun.criss.android@gmail.com>

---
+++ src/main/java/com/simplify/studySpringBoot/TestController.java
@@ -0,0 +1,28 @@
+package com.simplify.studySpringBoot;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/test")
+    public String testApi(){
+        return "Hello API";
+    }
+
+    @GetMapping(value = "/{firstPath}/test/{secondPath}")
+    public String testApiWithPathVariable(@PathVariable() String firstPath,
+                                          @PathVariable() String secondPath){
+        return "Hello API - variables: 1)" + firstPath + " 2)" + secondPath;
+    }
+
+    @GetMapping(value = "/test2")
+    public String testApiWithRequestParam(@RequestParam String abc){
+        return "Hello API - requestParam: abc=" + abc;
+    }
+
+    @PostMapping(value = "/testpost")
+    public String testApiPost(@RequestBody String body){
+        return "Hello API - requestParam: body=" + body;
+    }
+}
